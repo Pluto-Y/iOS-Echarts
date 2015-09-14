02941e8b2d4d554575c9f7edb4d11077a5afb4df
Pluto Y <kuaileainits@163.com>
2015-09-15 00:32:39 +0800

    add method and property into Tooltip Model

--- iOS-Echarts/Model/PYTooltip.h
+++ iOS-Echarts/Model/PYTooltip.h
@@ -7,7 +7,40 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "PYAxisPointer.h"
+
+typedef enum {
+    PYTooltipTriggerItem,
+    PYTooltipTriggerAxis
+} PYTooltipTrigger;
 
 @interface PYTooltip : NSObject
+
+@property (assign, nonatomic) BOOL show;
+@property (retain, nonatomic) NSNumber *zlevel;
+@property (retain, nonatomic) NSNumber *z;
+@property (assign, nonatomic) BOOL showContent;
+@property (retain, nonatomic, readonly, getter=getTrigger) NSString *trigger;
+@property (retain, nonatomic) id position;
+@property (retain, nonatomic) id formatter;
+@property (retain, nonatomic) id islandFormmater;
+@property (retain, nonatomic) NSNumber *showDelay;
+@property (retain, nonatomic) NSNumber *hideDelay;
+@property (retain, nonatomic) NSNumber *transitionDuration;
+@property (assign, nonatomic) BOOL enterable;
+@property (retain, nonatomic) PYColor *backgroundColor;
+@property (retain, nonatomic) PYColor *borderColor;
+@property (retain, nonatomic) NSNumber *borderRadius;
+@property (retain, nonatomic) NSNumber *borderWidth;
+@property (retain, nonatomic) id padding;
+@property (retain, nonatomic) PYAxisPointer *axisPointer;
+@property (retain, nonatomic) PYTextStyle *textStyle;
+
+/**
+ *  设置触发类型
+ *
+ *  @param pyTooltipTrigger 触发类型
+ */
+-(void)setPYTooltipTrigger:(PYTooltipTrigger) pyTooltipTrigger;
 
 @end
