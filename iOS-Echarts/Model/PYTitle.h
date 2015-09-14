f14fe7fb7e7a72a6dd37a6cc8bbd2583949d216f
Pluto Y <kuaileainits@163.com>
2015-09-14 23:55:54 +0800

    添加PYTitle类，并初始化好属性

---
+++ iOS-Echarts/Model/PYTitle.h
@@ -0,0 +1,33 @@
+//
+//  PYTitle.h
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/9/14.
+//  Copyright (c) 2015年 pluto-y. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PYTitle : NSObject
+
+@property (assign, nonatomic) BOOL show;
+@property (retain, nonatomic) NSNumber *zlevel;
+@property (retain, nonatomic) NSNumber *z;
+@property (retain, nonatomic) NSString *text;
+@property (retain, nonatomic) NSString *link;
+@property (retain, nonatomic) NSString *target;
+@property (retain, nonatomic) NSString *subtext;
+@property (retain, nonatomic) NSString *sublink;
+@property (retain, nonatomic) NSString *subtarget;
+@property (retain, nonatomic) id x;
+@property (retain, nonatomic) id y;
+@property (retain, nonatomic) NSString *textAlign;
+@property (retain, nonatomic) PYColor *backgroundColor;
+@property (retain, nonatomic) PYColor *borderColor;
+@property (retain, nonatomic) NSNumber *borderWidth;
+@property (retain, nonatomic) id padding;
+@property (retain, nonatomic) NSNumber *itemGap;
+@property (retain, nonatomic) PYTextStyle *textStyle;
+@property (retain, nonatomic) PYTextStyle *subtextStyle;
+
+@end
---
+++ iOS-Echarts/Model/PYTitle.m
@@ -0,0 +1,41 @@
+//
+//  PYTitle.m
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/9/14.
+//  Copyright (c) 2015年 pluto-y. All rights reserved.
+//
+
+#import "PYTitle.h"
+
+@implementation PYTitle
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _show = YES;
+        _zlevel = @(0);
+        _z = @(0);
+        _text = @"";
+        _link = @"";
+        _subtext = @"";
+        _sublink = @"";
+        _x = @"left";
+        _y = @"top";
+        _backgroundColor = PYRGBA(0, 0, 0, 0);
+        _borderColor = PYRGBA(12, 12, 12, 1);
+        _borderWidth = @(0);
+        _padding = @(5);
+        _itemGap = @(5);
+        _textStyle = [[PYTextStyle alloc] init];
+        _textStyle.fontSize = @(18);
+        _textStyle.fontWeight = @"bolder";
+        _textStyle.color = PYRGBA(3, 3, 3, 1);
+        _subtextStyle = [[PYTextStyle alloc] init];
+        _subtextStyle.color = PYRGBA(10, 10, 10, 1);
+    }
+    return self;
+}
+
+@end
