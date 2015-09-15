7be6542f79bb2d061972f507933579b572afc6ec
Pluto Y <kuaileainits@163.com>
2015-09-16 00:37:56 +0800

    Add MarkLine Model
    Add MarkLine Object with properties and methods for Series.

---
+++ iOS-Echarts/Model/Series/PYMarkLine.h
@@ -0,0 +1,47 @@
+//
+//  PYMarkLine.h
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/9/16.
+//  Copyright (c) 2015年 pluto-y. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PYBundling : NSObject
+
+@property (assign, nonatomic) BOOL enable;
+@property (retain, nonatomic) NSNumber *maxTurningAngle;
+
+@end
+
+@interface PYEffect : NSObject
+
+@property (assign, nonatomic) BOOL show;
+@property (assign, nonatomic) BOOL loop;
+@property (retain, nonatomic) NSNumber *period;
+@property (retain, nonatomic) NSNumber *scaleSize;
+@property (retain, nonatomic) PYColor *color;
+@property (retain, nonatomic) PYColor *shadowColor;
+@property (retain, nonatomic) NSNumber *shadowBlur;
+
+@end
+
+
+@interface PYMarkLine : NSObject
+
+@property (assign, nonatomic) BOOL clickable;
+@property (retain, nonatomic) id symbol;
+@property (retain, nonatomic) id symbolSize;
+@property (retain, nonatomic) id symbolRotate;
+@property (assign, nonatomic) BOOL large;
+@property (assign, nonatomic) BOOL smooth;
+@property (retain, nonatomic) NSNumber *smoothness;
+@property (retain, nonatomic) NSNumber *precision;
+@property (retain, nonatomic) PYBundling *bundling;
+@property (retain, nonatomic) PYEffect *effect;
+@property (retain, nonatomic) PYItemStyle *itemStyle;
+@property (retain, nonatomic) NSArray *data;
+
+
+@end
---
+++ iOS-Echarts/Model/Series/PYMarkLine.m
@@ -0,0 +1,47 @@
+//
+//  PYMarkLine.m
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/9/16.
+//  Copyright (c) 2015年 pluto-y. All rights reserved.
+//
+
+#import "PYMarkLine.h"
+
+@implementation PYBundling
+
+
+
+@end
+
+@implementation PYEffect
+
+
+@end
+
+@implementation PYMarkLine
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _clickable = YES;
+        _symbol = @[@"circle", @"arrow"];
+        _symbolSize = @[@(2), @(4)];
+        _large = NO;
+        _smooth = NO;
+        _smoothness = @(0.2);
+        _precision = @(2);
+        _bundling = [[PYBundling alloc] init];
+        _bundling.enable = NO;
+        _bundling.maxTurningAngle = @(45);
+        _effect = [[PYEffect alloc] init];
+        _effect.show = NO;
+        _effect.loop = YES;
+        _effect.period = @(15);
+        _effect.scaleSize = @(2);
+    }
+    return self;
+}
+
+@end
