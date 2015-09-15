9538cfca2ff9513877c38584c12996ae46c952c1
Pluto Y <kuaileainits@163.com>
2015-09-16 00:23:26 +0800

    Add Toolbox Model
    Add Toolbox Properties and init method

---
+++ iOS-Echarts/Model/PYToolbox.h
@@ -0,0 +1,32 @@
+//
+//  PYToolbox.h
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/9/16.
+//  Copyright (c) 2015年 pluto-y. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PYToolbox : NSObject
+
+@property (assign, nonatomic) BOOL show;
+@property (retain, nonatomic) NSNumber *zlevel;
+@property (retain, nonatomic) NSNumber *z;
+@property (retain, nonatomic) NSString *orient;
+@property (retain, nonatomic) id x;
+@property (retain, nonatomic) id y;
+@property (retain, nonatomic) PYColor *backgroundColor;
+@property (retain, nonatomic) PYColor *borderColor;
+@property (retain, nonatomic) NSNumber *borderWidth;
+@property (retain, nonatomic) id padding;
+@property (retain, nonatomic) NSNumber *itemGap;
+@property (retain, nonatomic) NSNumber *itemSize;
+@property (retain, nonatomic) NSArray *color;
+@property (retain, nonatomic) PYColor *disableColor;
+@property (retain, nonatomic) PYColor *effectiveColor;
+@property (assign, nonatomic) BOOL showTitle;
+@property (retain, nonatomic) PYTextStyle *textStyle;
+// PYFeature
+
+@end
---
+++ iOS-Echarts/Model/PYToolbox.m
@@ -0,0 +1,37 @@
+//
+//  PYToolbox.m
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/9/16.
+//  Copyright (c) 2015年 pluto-y. All rights reserved.
+//
+
+#import "PYToolbox.h"
+
+@implementation PYToolbox
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _show = NO;
+        _zlevel = @(0);
+        _z = @(6);
+        _orient = @"horizontal";
+        _x = @"right";
+        _y = @"y";
+        _backgroundColor = PYRGBA(0, 0, 0, 0);
+        _borderColor = PYRGBA(12, 12, 12, 1);
+        _borderWidth = @(0);
+        _padding = @(5);
+        _itemGap = @(10);
+        _itemSize = @(16);
+        _color = @[PYRGBA(30, 144, 255, 1), PYRGBA(34, 187, 34, 1), PYRGBA(75, 0, 130, 1), PYRGBA(210, 105, 30, 1)];
+        _disableColor = PYRGBA(13, 13, 13, 0);
+        _effectiveColor = PYRGBA(255, 0, 0, 1);
+        _showTitle = YES;
+    }
+    return self;
+}
+
+@end
