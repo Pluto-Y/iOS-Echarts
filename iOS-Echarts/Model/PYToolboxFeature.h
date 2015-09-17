b090e92537b8246da27124ea7185efcf5c581a4a
Pluto Y <kuaileainits@163.com>
2015-09-18 00:18:10 +0800

    Add new Model
    Add PYToolboxFeature for Toolbox

---
+++ iOS-Echarts/Model/PYToolboxFeature.h
@@ -0,0 +1,14 @@
+//
+//  PYToolboxFeature.h
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/9/18.
+//  Copyright (c) 2015年 pluto-y. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PYToolboxFeature : NSObject
+
+
+@end
---
+++ iOS-Echarts/Model/PYToolboxFeature.m
@@ -0,0 +1,13 @@
+//
+//  PYToolboxFeature.m
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/9/18.
+//  Copyright (c) 2015年 pluto-y. All rights reserved.
+//
+
+#import "PYToolboxFeature.h"
+
+@implementation PYToolboxFeature
+
+@end
