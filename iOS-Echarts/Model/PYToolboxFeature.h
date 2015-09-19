c0f0c6b30f93c2e37fd3ef9422b3eb0c41134244
Pluto Y <kuaileainits@163.com>
2015-09-19 23:58:11 +0800

    Add Feature Model for Toolbox
    Add properties, inner classes and methods for Feature.

--- iOS-Echarts/Model/PYToolboxFeature.h
+++ iOS-Echarts/Model/PYToolboxFeature.h
@@ -8,7 +8,92 @@
 
 #import <Foundation/Foundation.h>
 
+@interface PYToolboxFeatureMarkTitle : NSObject
+
+@property (retain, nonatomic) NSString *mark;
+@property (retain, nonatomic) NSString *markUndo;
+@property (retain, nonatomic) NSString *markClear;
+
+@end
+
+@interface PYToolboxFeatureMark : NSObject
+
+@property (assign, nonatomic) BOOL show;
+@property (retain, nonatomic) PYToolboxFeatureMarkTitle *title;
+@property (retain, nonatomic) PYLineStyle *lineStyle;
+
+@end
+
+@interface PYToolboxFeatureDataZoomTitle : NSObject
+
+@property (retain, nonatomic) NSString *dataZoom;
+@property (retain, nonatomic) NSString *dataZoomReset;
+
+@end
+
+@interface PYToolboxFeatureDataZoom : NSObject
+
+@property (assign, nonatomic) BOOL show;
+@property (retain, nonatomic) PYToolboxFeatureDataZoomTitle *title;
+
+@end
+
+@interface PYToolboxFeatureDataView : NSObject
+
+@property (assign, nonatomic) BOOL show;
+@property (retain, nonatomic) NSString *title;
+@property (assign, nonatomic) BOOL readOnly;
+@property (retain, nonatomic) NSArray *lang;
+
+@end
+
+@interface PYToolboxFeatureMagicTypeTitle : NSObject
+
+@property (retain, nonatomic) NSString *line;
+@property (retain, nonatomic) NSString *bar;
+@property (retain, nonatomic) NSString *stack;
+@property (retain, nonatomic) NSString *tiled;
+@property (retain, nonatomic) NSString *force;
+@property (retain, nonatomic) NSString *chord;
+@property (retain, nonatomic) NSString *pie;
+@property (retain, nonatomic) NSString *funnel;
+
+@end
+
+@interface PYToolboxFeatureMagicType : NSObject
+
+@property (assign, nonatomic) BOOL show;
+@property (retain, nonatomic) PYToolboxFeatureMagicTypeTitle *title;
+//@property    缺少option
+@property (retain, nonatomic) NSArray *type;
+
+@end
+
+@interface PYToolboxFeatureRestore : NSObject
+
+@property (assign, nonatomic) BOOL show;
+@property (retain, nonatomic) NSString *title;
+
+@end
+
+@interface PYToolboxFeatureSaveAsImage : NSObject
+
+@property (assign, nonatomic) BOOL show;
+@property (retain, nonatomic) NSString *title;
+@property (retain, nonatomic) NSString *type;
+@property (retain, nonatomic) NSString *lang;
+
+@end
+
+
 @interface PYToolboxFeature : NSObject
+
+@property (retain, nonatomic) PYToolboxFeatureMark *mark;
+@property (retain, nonatomic) PYToolboxFeatureDataZoom *dataZoom;
+@property (retain, nonatomic) PYToolboxFeatureDataView *dataView;
+@property (retain, nonatomic) PYToolboxFeatureMagicType *magicType;
+@property (retain, nonatomic) PYToolboxFeatureRestore *restore;
+@property (retain, nonatomic) PYToolboxFeatureSaveAsImage *saveAsImage;
 
 
 @end
--- iOS-Echarts/Model/PYToolboxFeature.m
+++ iOS-Echarts/Model/PYToolboxFeature.m
@@ -8,6 +8,92 @@
 
 #import "PYToolboxFeature.h"
 
+@implementation PYToolboxFeatureMarkTitle
+
+@end
+
+@implementation PYToolboxFeatureMark
+
+@end
+
+@implementation PYToolboxFeatureDataZoomTitle
+
+
+@end
+
+@implementation PYToolboxFeatureDataZoom
+
+@end
+
+@implementation PYToolboxFeatureDataView
+
+@end
+
+@implementation PYToolboxFeatureMagicTypeTitle
+
+@end
+
+@implementation PYToolboxFeatureMagicType
+
+@end
+
+@implementation PYToolboxFeatureRestore
+
+@end
+
+@implementation PYToolboxFeatureSaveAsImage
+
+@end
+
+
+
 @implementation PYToolboxFeature
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _mark = [[PYToolboxFeatureMark alloc] init];
+        _mark.show = NO;
+        _mark.title = [[PYToolboxFeatureMarkTitle alloc] init];
+        _mark.title.mark = @"辅助线开关";
+        _mark.title.markUndo = @"删除辅助线";
+        _mark.title.markClear = @"清空辅助线";
+        _mark.lineStyle = [[PYLineStyle alloc] init];
+        _mark.lineStyle.width = @(2);
+        _mark.lineStyle.color = PYRGBA(30, 144, 255, 1);
+        [_mark.lineStyle setLineStyleType:PYLineStyleTypeDashed];
+        _dataZoom = [[PYToolboxFeatureDataZoom alloc] init];
+        _dataZoom.show = YES;
+        _dataZoom.title = [[PYToolboxFeatureDataZoomTitle alloc] init];
+        _dataZoom.title.dataZoom = @"区域缩放";
+        _dataZoom.title.dataZoomReset = @"区域缩放后退";
+        _dataView = [[PYToolboxFeatureDataView alloc] init];
+        _dataView.show = NO;
+        _dataView.title = @"数据视图";
+        _dataView.readOnly = NO;
+        _dataView.lang = @[@"数据视图", @"关闭", @"刷新"];
+        _magicType = [[PYToolboxFeatureMagicType alloc] init];
+        _magicType.show = NO;
+        _magicType.title = [[PYToolboxFeatureMagicTypeTitle alloc] init];
+        _magicType.title.line = @"折线图切换";
+        _magicType.title.bar = @"柱形图切换";
+        _magicType.title.stack = @"堆积";
+        _magicType.title.tiled = @"平铺";
+        _magicType.title.force = @"力导向布局图切换";
+        _magicType.title.chord = @"和弦图切换";
+        _magicType.title.pie = @"饼图切换";
+        _magicType.title.funnel = @"漏斗图切换";
+        _restore = [[PYToolboxFeatureRestore alloc] init];
+        _restore.show = NO;
+        _restore.title = @"还原";
+        _saveAsImage = [[PYToolboxFeatureSaveAsImage alloc] init];
+        _saveAsImage.show = NO;
+        _saveAsImage.title = @"保存为图片";
+        _saveAsImage.type = @"png";
+        _saveAsImage.lang = @"点击保存";
+    }
+    return self;
+}
 
 @end
