6d3b446bab82bd51542dd4649e292026c8b82bb2
Pluto Y <kuaileainits@163.com>
2015-10-02 13:10:21 +0800

    修改之前自动适配的代码
    修改之前放大的视图的代码，建议之后用Grid的方法来进行调整坐标轴

--- iOS-Echarts/PYEchartsView.m
+++ iOS-Echarts/PYEchartsView.m
@@ -90,107 +90,107 @@
 
 /**
  *  根据
- *
- *  @param webView <#webView description#>
  */
 -(void)resizeDiv {
-    float height = self.frame.size.height + DEFAULT_HEIGHT_DIFF_CONSTANT - _padding.top - _padding.bottom;
-    float width = self.frame.size.width + DEFAULT_WIDTH_DIFF_CONSTANT - _padding.left - _padding.right;
-    float paddingLeft = _padding.left + DEFAULT_LEFT_MARGIN_DIFF_CONSTANT;
-    float paddingTop = _padding.top + DEFAULT_TOP_MARGIN_DIFF_CONSTANT;
-    float paddingRight = _padding.right;
-    float paddingBottom = _padding.bottom;
-    float minLeft = 0.0; // title或legend哪个的x最小
-    float minTop = 0.0;
-    BOOL hasRight = NO;
-    if(option != nil) {
-        if (option.title != nil && option.title.show) {
-            if ([option.title.x isKindOfClass:[NSString class]]) {
-                if ([option.title.x isEqualToString:@"left"]) {
-                    minLeft = 0.0;
-                } else if ([option.title.x isEqualToString:@"right"]) {
-                    hasRight = YES;
-                }
-                
-            } else if ([option.title.x isKindOfClass:[NSNumber class]]) {
-                minLeft = [option.title.x floatValue];
-            }
-            if ([option.title.y isKindOfClass:[NSString class]] && [option.title.y isEqualToString:@"top"]) {
-                minTop = 0.0;
-            } else if ([option.title.y isKindOfClass:[NSNumber class]]) {
-                minTop = [option.title.y floatValue];
-            }
-        }
-        if (option.legend != nil && option.legend.show) {
-            if([option.legend.x isKindOfClass:[NSString class]]) {
-                if ([option.legend.x isEqualToString:@"left"]) {
-                    minLeft = 0.0;
-                } else if ([option.legend.x isEqualToString:@"right"]) {
-                    hasRight = YES;
-                }
-            } else if ([option.legend.x isKindOfClass:[NSNumber class]]) {
-                if ([option.legend.x floatValue] < minLeft) {
-                    minLeft = [option.legend.x floatValue];
-                }
-            }
-            if ([option.legend.y isKindOfClass:[NSString class]] && [option.legend.y isEqualToString:@"top"]) {
-                minTop = 0.0;
-            } else if ([option.legend.y isKindOfClass:[NSNumber class]]) {
-                if ([option.legend.y floatValue] < minTop) {
-                    minTop = [option.legend.y floatValue];
-                }
-            }
-        }
-        if (option.toolbox != nil && option.toolbox.show) {
-            if ([option.toolbox.x isKindOfClass:[NSString class]]) {
-                if ([option.toolbox.x isEqualToString:@"left"]) {
-                    minLeft = 0.0;
-                } else if ([option.toolbox.x isEqualToString:@"right"]) {
-                    hasRight = YES;
-                }
-            } else if ([option.toolbox.x isKindOfClass:[NSNumber class]]) {
-                if ([option.toolbox.x floatValue] < minLeft) {
-                    minLeft = [option.toolbox.x floatValue];
-                }
-            }
-            if ([option.toolbox.y isKindOfClass:[NSString class]] && [option.toolbox.y isEqualToString:@"top"]) {
-                minTop = 0.0;
-            } else if ([option.toolbox.y isKindOfClass:[NSNumber class]]) {
-                if ([option.toolbox.y floatValue] < minTop) {
-                    minTop = [option.toolbox.y floatValue];
-                }
-            }
-        }
-        if (minLeft > 0) {
-            if (minLeft < DEFAULT_HEAD_LEFT_PADDING) {
-                paddingLeft += DEFAULT_HEAD_LEFT_PADDING - minLeft;
-                width -= DEFAULT_HEAD_LEFT_PADDING - minLeft;
-            }
-        } else {
-            paddingLeft += DEFAULT_HEAD_LEFT_PADDING;
-            width -= DEFAULT_HEAD_LEFT_PADDING;
-        }
-        
-        if (minTop > 0) {
-            if (minTop < DEFAULT_HEAD_TOP_PADDING) {
-                paddingTop += DEFAULT_HEAD_TOP_PADDING - minTop;
-                height -= DEFAULT_HEAD_TOP_PADDING - minTop;
-            }
-        } else {
-            paddingTop += DEFAULT_HEAD_TOP_PADDING;
-            height -= DEFAULT_HEAD_TOP_PADDING;
-        }
-        if (hasRight) {
-            paddingRight += DEFAULT_HEAD_RIGHT_PADDING;
-            width -= DEFAULT_HEAD_RIGHT_PADDING;
-        }
-        if (option.dataZoom != nil && option.dataZoom.show) {
-            paddingBottom += DEFAULT_BOTTOM_PADDING;
-            height -= DEFAULT_BOTTOM_PADDING;
-        }
-    }
-    
-    NSString *divSizeCss = [NSString stringWithFormat:@"'height:%.0fpx;width:%.0fpx;margin-left:%.0fpx;margin-top:%.0fpx;margin-right:%.0fpx;margin-bottom:%.0fpx;'", height, width, paddingLeft, paddingTop, paddingRight, paddingBottom] ;
+//    float height = self.frame.size.height + DEFAULT_HEIGHT_DIFF_CONSTANT - _padding.top - _padding.bottom;
+//    float width = self.frame.size.width + DEFAULT_WIDTH_DIFF_CONSTANT - _padding.left - _padding.right;
+//    float paddingLeft = _padding.left + DEFAULT_LEFT_MARGIN_DIFF_CONSTANT;
+//    float paddingTop = _padding.top + DEFAULT_TOP_MARGIN_DIFF_CONSTANT;
+//    float paddingRight = _padding.right;
+//    float paddingBottom = _padding.bottom;
+//    float minLeft = 0.0; // title或legend哪个的x最小
+//    float minTop = 0.0;
+//    BOOL hasRight = NO;
+//    if(option != nil) {
+//        if (option.title != nil && option.title.show) {
+//            if ([option.title.x isKindOfClass:[NSString class]]) {
+//                if ([option.title.x isEqualToString:@"left"]) {
+//                    minLeft = 0.0;
+//                } else if ([option.title.x isEqualToString:@"right"]) {
+//                    hasRight = YES;
+//                }
+//                
+//            } else if ([option.title.x isKindOfClass:[NSNumber class]]) {
+//                minLeft = [option.title.x floatValue];
+//            }
+//            if ([option.title.y isKindOfClass:[NSString class]] && [option.title.y isEqualToString:@"top"]) {
+//                minTop = 0.0;
+//            } else if ([option.title.y isKindOfClass:[NSNumber class]]) {
+//                minTop = [option.title.y floatValue];
+//            }
+//        }
+//        if (option.legend != nil && option.legend.show) {
+//            if([option.legend.x isKindOfClass:[NSString class]]) {
+//                if ([option.legend.x isEqualToString:@"left"]) {
+//                    minLeft = 0.0;
+//                } else if ([option.legend.x isEqualToString:@"right"]) {
+//                    hasRight = YES;
+//                }
+//            } else if ([option.legend.x isKindOfClass:[NSNumber class]]) {
+//                if ([option.legend.x floatValue] < minLeft) {
+//                    minLeft = [option.legend.x floatValue];
+//                }
+//            }
+//            if ([option.legend.y isKindOfClass:[NSString class]] && [option.legend.y isEqualToString:@"top"]) {
+//                minTop = 0.0;
+//            } else if ([option.legend.y isKindOfClass:[NSNumber class]]) {
+//                if ([option.legend.y floatValue] < minTop) {
+//                    minTop = [option.legend.y floatValue];
+//                }
+//            }
+//        }
+//        if (option.toolbox != nil && option.toolbox.show) {
+//            if ([option.toolbox.x isKindOfClass:[NSString class]]) {
+//                if ([option.toolbox.x isEqualToString:@"left"]) {
+//                    minLeft = 0.0;
+//                } else if ([option.toolbox.x isEqualToString:@"right"]) {
+//                    hasRight = YES;
+//                }
+//            } else if ([option.toolbox.x isKindOfClass:[NSNumber class]]) {
+//                if ([option.toolbox.x floatValue] < minLeft) {
+//                    minLeft = [option.toolbox.x floatValue];
+//                }
+//            }
+//            if ([option.toolbox.y isKindOfClass:[NSString class]] && [option.toolbox.y isEqualToString:@"top"]) {
+//                minTop = 0.0;
+//            } else if ([option.toolbox.y isKindOfClass:[NSNumber class]]) {
+//                if ([option.toolbox.y floatValue] < minTop) {
+//                    minTop = [option.toolbox.y floatValue];
+//                }
+//            }
+//        }
+//        if (minLeft > 0) {
+//            if (minLeft < DEFAULT_HEAD_LEFT_PADDING) {
+//                paddingLeft += DEFAULT_HEAD_LEFT_PADDING - minLeft;
+//                width -= DEFAULT_HEAD_LEFT_PADDING - minLeft;
+//            }
+//        } else {
+//            paddingLeft += DEFAULT_HEAD_LEFT_PADDING;
+//            width -= DEFAULT_HEAD_LEFT_PADDING;
+//        }
+//        
+//        if (minTop > 0) {
+//            if (minTop < DEFAULT_HEAD_TOP_PADDING) {
+//                paddingTop += DEFAULT_HEAD_TOP_PADDING - minTop;
+//                height -= DEFAULT_HEAD_TOP_PADDING - minTop;
+//            }
+//        } else {
+//            paddingTop += DEFAULT_HEAD_TOP_PADDING;
+//            height -= DEFAULT_HEAD_TOP_PADDING;
+//        }
+//        if (hasRight) {
+//            paddingRight += DEFAULT_HEAD_RIGHT_PADDING;
+//            width -= DEFAULT_HEAD_RIGHT_PADDING;
+//        }
+//        if (option.dataZoom != nil && option.dataZoom.show) {
+//            paddingBottom += DEFAULT_BOTTOM_PADDING;
+//            height -= DEFAULT_BOTTOM_PADDING;
+//        }
+//    }
+    float height = self.frame.size.height;
+    float width = self.frame.size.width;
+//    NSString *divSizeCss = [NSString stringWithFormat:@"'height:%.0fpx;width:%.0fpx;margin-left:%.0fpx;margin-top:%.0fpx;margin-right:%.0fpx;margin-bottom:%.0fpx;'", height, width, paddingLeft, paddingTop, paddingRight, paddingBottom] ;
+    NSString *divSizeCss = [NSString stringWithFormat:@"'height:%.0fpx;width:%.0fpx;'", height, width];
     NSString *js = [NSString stringWithFormat:@"%@(%@)", @"resizeDiv", divSizeCss];
     [self stringByEvaluatingJavaScriptFromString:js];
     NSLog(@"cssText:%@", divSizeCss);
