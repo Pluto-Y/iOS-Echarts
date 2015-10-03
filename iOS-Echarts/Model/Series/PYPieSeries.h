6d33a6e6c7a67f96dbdc6ca3a03eefa542270b42
Pluto Y <kuaileainits@163.com>
2015-10-04 02:53:07 +0800

    添加饼图的Series对象
    添加饼图的Series对象

---
+++ iOS-Echarts/Model/Series/PYPieSeries.h
@@ -0,0 +1,23 @@
+//
+//  PYPieSeries.h
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/10/3.
+//  Copyright © 2015年 pluto-y. All rights reserved.
+//
+
+#import "PYSeries.h"
+
+@interface PYPieSeries : PYSeries
+
+@property (retain, nonatomic) NSArray *center;
+@property (retain, nonatomic) id radius;
+@property (retain, nonatomic) NSNumber *startAngle;
+@property (retain, nonatomic) NSNumber *minAngle;
+@property (assign, nonatomic) BOOL clockWise;
+@property (retain, nonatomic) NSString *roseType;
+@property (retain, nonatomic) NSNumber *selectedOffset;
+@property (retain, nonatomic) id selectedMode;
+@property (assign, nonatomic) BOOL legendHoverLink;
+
+@end
---
+++ iOS-Echarts/Model/Series/PYPieSeries.m
@@ -0,0 +1,28 @@
+//
+//  PYPieSeries.m
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/10/3.
+//  Copyright © 2015年 pluto-y. All rights reserved.
+//
+
+#import "PYPieSeries.h"
+
+@implementation PYPieSeries
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _center = @[@"50%", @"50%"];
+        _radius = @[@"0", @"75%"];
+        _startAngle = @(90);
+        _minAngle = @(0);
+        _clockWise = YES;
+        _selectedMode = @(10);
+        _legendHoverLink = YES;
+    }
+    return self;
+}
+
+@end
