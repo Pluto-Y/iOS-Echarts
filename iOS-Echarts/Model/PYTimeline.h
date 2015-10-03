83d029450e7d347ac986ac63214c772fd1e5abb9
Pluto Y <kuaileainits@163.com>
2015-10-04 02:53:18 +0800

    添加时间轴对象
    添加时间轴对象

---
+++ iOS-Echarts/Model/PYTimeline.h
@@ -0,0 +1,72 @@
+//
+//  PYTimeline.h
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/10/4.
+//  Copyright © 2015年 pluto-y. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PYTimelineLabel : NSObject
+
+@property (assign, nonatomic) BOOL show;
+@property (retain, nonatomic) id interval;
+@property (retain, nonatomic) NSNumber *rotate;
+@property (retain, nonatomic) id formatter;
+@property (retain, nonatomic) PYTextStyle *textStyle;
+
+@end
+
+@interface PYTimelineCheckpointStyle : NSObject
+
+@property (retain, nonatomic) NSString *symbol;
+@property (retain, nonatomic) id symbolSize;
+@property (retain, nonatomic) PYColor *color;
+@property (retain, nonatomic) PYColor *borderColor;
+@property (retain, nonatomic) id borderWidth;
+@property (retain, nonatomic) PYTimelineLabel *label;
+
+@end
+
+@interface PYTimelineControlStyle : NSObject
+
+@property (retain, nonatomic) NSNumber *itemSize;
+@property (retain, nonatomic) NSNumber *itemGap;
+@property (retain, nonatomic) NSDictionary *normal;
+@property (retain, nonatomic) NSDictionary *emphasis;
+
+@end
+
+@interface PYTimeline : NSObject
+
+@property (assign, nonatomic) BOOL show;
+@property (retain, nonatomic) NSNumber *zlevel;
+@property (retain, nonatomic) NSNumber *z;
+@property (retain, nonatomic) NSString *type;
+@property (assign, nonatomic) BOOL notMerge;
+@property (assign, nonatomic) BOOL realtime;
+@property (retain, nonatomic) id x;
+@property (retain, nonatomic) id y;
+@property (retain, nonatomic) id x2;
+@property (retain, nonatomic) id y2;
+@property (retain, nonatomic) NSNumber *width;
+@property (retain, nonatomic) NSNumber *height;
+@property (retain, nonatomic) PYColor *backgroundColor;
+@property (retain, nonatomic) NSNumber *borderWidth;
+@property (retain, nonatomic) PYColor *borderColor;
+@property (retain, nonatomic) id padding;
+@property (retain, nonatomic) NSString *controlPosition;
+@property (assign, nonatomic) BOOL autoPlay;
+@property (assign, nonatomic) BOOL loop;
+@property (retain, nonatomic) NSNumber *playInterval;
+@property (retain, nonatomic) PYLineStyle *lineStyle;
+@property (retain, nonatomic) PYTimelineLabel *label;
+@property (retain, nonatomic) PYTimelineCheckpointStyle *checkpointStyle;
+@property (retain, nonatomic) PYTimelineControlStyle *controlStyle;
+@property (retain, nonatomic) NSString *symbol;
+@property (retain, nonatomic) NSNumber *symbolSize;
+@property (retain, nonatomic) NSNumber *currentIndex;
+@property (retain, nonatomic) NSMutableArray *data;
+
+@end
---
+++ iOS-Echarts/Model/PYTimeline.m
@@ -0,0 +1,99 @@
+//
+//  PYTimeline.m
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/10/4.
+//  Copyright © 2015年 pluto-y. All rights reserved.
+//
+
+#import "PYTimeline.h"
+
+@implementation PYTimelineLabel
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _show = YES;
+        _interval = @"auto";
+        _rotate = @(0);
+        _textStyle = [[PYTextStyle alloc] init];
+        _textStyle.color = PYRGBA(3, 3, 3, 1);
+    }
+    return self;
+}
+
+@end
+
+@implementation PYTimelineCheckpointStyle
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _symbol = @"auto";
+        _symbolSize = @"auto";
+        _borderWidth = @"auto";
+        _label = [[PYTimelineLabel alloc] init];
+        _label.show = NO;
+    }
+    return self;
+}
+
+@end
+
+@implementation PYTimelineControlStyle
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _itemSize = @(15);
+        _itemGap = @(5);
+        _normal = @{@"color" : @"#333"};
+        _emphasis = @{@"color" : @"#1e90ff"};
+    }
+    return self;
+}
+
+@end
+
+@implementation PYTimeline
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _show = YES;
+        _zlevel = @(0);
+        _z = @(4);
+        _type = @"time";
+        _notMerge = NO;
+        _realtime = YES;
+        _x = @(80);
+        _x2 = @(80);
+        _y2 = @(0);
+        _height = @(50);
+        _backgroundColor = PYRGBA(0, 0, 0, 0);
+        _borderWidth = @(0);
+        _borderColor = PYRGBA(12, 12, 12, 1);
+        _padding = @(5);
+        _controlPosition = @"left";
+        _autoPlay = NO;
+        _loop = YES;
+        _playInterval = @(2000);
+        _lineStyle = [[PYLineStyle alloc] init];
+        _lineStyle.color = PYRGBA(6, 6, 6, 1);
+        _lineStyle.width = @(1);
+        _lineStyle.type = @"dashed";
+        _label = [[PYTimelineLabel alloc] init];
+        _checkpointStyle = [[PYTimelineCheckpointStyle alloc] init];
+        _controlStyle = [[PYTimelineControlStyle alloc] init];
+        _symbol = @"emptyDiamond";
+        _symbolSize = @(4);
+        _currentIndex = @(0);
+    }
+    return self;
+}
+
+@end
