adf63070cd9c28db985810af774797770b6db0ac
Pluto Y <kuaileainits@163.com>
2015-11-23 10:29:02 +0800

    搭好散点图的框架
    搭好散点图的框架

---
+++ iOS-Echarts_Demo/Controller/ScatterDemoController.h
@@ -0,0 +1,17 @@
+//
+//  ScatterDemoController.h
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/11/23.
+//  Copyright © 2015年 pluto-y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PYEchartsView.h"
+
+
+@interface ScatterDemoController : UIViewController
+
+@property (strong, nonatomic) IBOutlet PYEchartsView *kEchartView;
+
+@end
---
+++ iOS-Echarts_Demo/Controller/ScatterDemoController.m
@@ -0,0 +1,31 @@
+//
+//  ScatterDemoController.m
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/11/23.
+//  Copyright © 2015年 pluto-y. All rights reserved.
+//
+
+#import "ScatterDemoController.h"
+
+@interface ScatterDemoController ()
+
+@end
+
+@implementation ScatterDemoController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self initAll];
+}
+
+#pragma mark - custom functions
+/**
+ *  初始化
+ */
+-(void)initAll {
+    self.title = @"散点图";
+    
+}
+
+@end
