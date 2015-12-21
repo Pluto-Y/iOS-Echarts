53a2a6624fc287b4d3a8963cf73aff69e40b7dab
Pluto Y <kuaileainits@163.com>
2015-12-21 12:43:37 +0800

    Add Radar Series

---
+++ iOS-Echarts/Model/Series/PYRadarSeries.h
@@ -0,0 +1,19 @@
+//
+//  PYRadarSeries.h
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/12/21.
+//  Copyright © 2015年 pluto-y. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PYRadarSeries : NSObject
+
+@property (retain, nonatomic) NSNumber *polarIndex;
+@property (retain, nonatomic) NSString *symbol;
+@property (retain, nonatomic) id symbolSize;
+@property (retain, nonatomic) NSNumber *symbolRotate;
+@property (assign, nonatomic) BOOL legendHoverLink;
+
+@end
---
+++ iOS-Echarts/Model/Series/PYRadarSeries.m
@@ -0,0 +1,24 @@
+//
+//  PYRadarSeries.m
+//  iOS-Echarts
+//
+//  Created by ChipSea on 15/12/21.
+//  Copyright © 2015年 pluto-y. All rights reserved.
+//
+
+#import "PYRadarSeries.h"
+
+@implementation PYRadarSeries
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _polarIndex = (0);
+        _symbolSize = @(2);
+        _legendHoverLink = YES;
+    }
+    return self;
+}
+
+@end
