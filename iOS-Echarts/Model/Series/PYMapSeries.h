34556c56db7d24b9264a08b5afa62e4f3377e54f
Pluto Y <kuaileainits@163.com>
2016-02-17 02:43:07 +0800

    Add new series for the Map
    Add new series for the Map

---
+++ iOS-Echarts/Model/Series/PYMapSeries.h
@@ -0,0 +1,28 @@
+//
+//  PYMapSeries.h
+//  iOS-Echarts
+//
+//  Created by ChipSea on 16/2/17.
+//  Copyright © 2016年 pluto-y. All rights reserved.
+//
+
+#import "PYSeries.h"
+
+@interface PYMapSeries : PYSeries
+
+@property (retain, nonatomic) id selectedMode;
+@property (retain, nonatomic) NSString *mapType;
+@property (assign, nonatomic) BOOL hoverable;
+@property (assign, nonatomic) BOOL dataRangeHoverLink;
+@property (retain, nonatomic) NSDictionary *mapLocation;
+@property (retain, nonatomic) NSString *mapValueCalculation;
+@property (retain, nonatomic) NSNumber *mapValuePrecision;
+@property (assign, nonatomic) BOOL showLegendSymbol;
+@property (retain, nonatomic) id roam;
+@property (retain, nonatomic) NSDictionary *scaleLimit;
+@property (retain, nonatomic) NSDictionary *nameMap;
+@property (retain, nonatomic) NSDictionary *textFixed;
+@property (retain, nonatomic) NSDictionary *geoCoord;
+@property (retain, nonatomic) NSDictionary *heatmap;
+
+@end
---
+++ iOS-Echarts/Model/Series/PYMapSeries.m
@@ -0,0 +1,28 @@
+//
+//  PYMapSeries.m
+//  iOS-Echarts
+//
+//  Created by ChipSea on 16/2/17.
+//  Copyright © 2016年 pluto-y. All rights reserved.
+//
+
+#import "PYMapSeries.h"
+
+@implementation PYMapSeries
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _mapType = @"China";
+        _hoverable = YES;
+        _dataRangeHoverLink = YES;
+        _mapLocation = @{@"x":@"center", @"y":@"center"};
+        _mapValueCalculation = @"sum";
+        _showLegendSymbol = YES;
+        _roam = @(NO);
+    }
+    return self;
+}
+
+@end
