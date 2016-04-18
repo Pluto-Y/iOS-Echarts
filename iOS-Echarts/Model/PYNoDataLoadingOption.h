7f1411d7134e72c7193c24663feb162a3a4f1964
Pluto Y <kuaileainits@163.com>
2016-04-18 23:57:47 +0800

    Add model `PYNoDataLoadingOption` for the no data screen

---
+++ iOS-Echarts/Model/PYNoDataLoadingOption.h
@@ -0,0 +1,21 @@
+//
+//  PYNoDataLoadingOption.h
+//  iOS-Echarts
+//
+//  Created by Pluto Y on 4/18/16.
+//  Copyright © 2016 pluto-y. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PYNoDataLoadingOption : NSObject
+
+@property (strong, nonatomic) NSString *text;
+@property (strong, nonatomic) id x;
+@property (strong, nonatomic) id y;
+@property (strong, nonatomic) PYTextStyle *textStyle;
+@property (strong, nonatomic) id effect;
+@property (strong, nonatomic) NSDictionary *effectOption;
+@property (strong, nonatomic) NSNumber *progress;
+
+@end
---
+++ iOS-Echarts/Model/PYNoDataLoadingOption.m
@@ -0,0 +1,25 @@
+//
+//  PYNoDataLoadingOption.m
+//  iOS-Echarts
+//
+//  Created by Pluto Y on 4/18/16.
+//  Copyright © 2016 pluto-y. All rights reserved.
+//
+
+#import "PYNoDataLoadingOption.h"
+
+@implementation PYNoDataLoadingOption
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _text = @"暂无数据";
+        _x = @"center";
+        _y = @"center";
+        _effect = @"bubble";
+    }
+    return self;
+}
+
+@end
