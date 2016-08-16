0efdf25cf0573eb4f32c8a172647ad3ec02cdc76
Pluto Y <kuaileainits@163.com>
2016-08-17 00:46:41 +0800

    Add second demo for the treemap

--- iOS-Echarts_Demo/Controller/TreemapDemoController.m
+++ iOS-Echarts_Demo/Controller/TreemapDemoController.m
@@ -88,6 +88,52 @@ typedef NS_ENUM(NSInteger, TreemapDemoTypeTag) {
 }
 
 - (void)showTreemapDemo2 {
+    PYOption *option = [[PYOption alloc] init];
+    option.title = [[PYTitle alloc] init];
+    option.title.text = @"手机占有率";
+    option.title.subtext = @"数据下钻例子，虚构数据";
+    option.tooltip = [[PYTooltip alloc] init];
+    option.tooltip.trigger = PYTooltipTriggerItem;
+    option.tooltip.formatter = @"{b}: {c}";
+    option.toolbox = [[PYToolbox alloc] init];
+    option.toolbox.show = YES;
+    option.toolbox.feature = [[PYToolboxFeature alloc] init];
+    option.toolbox.feature.mark = [[PYToolboxFeatureMark alloc] init];
+    option.toolbox.feature.mark.show = YES;
+    option.toolbox.feature.dataView = [[PYToolboxFeatureDataView alloc] init];
+    option.toolbox.feature.dataView.show = YES;
+    option.toolbox.feature.dataView.readOnly = NO;
+    option.toolbox.feature.restore = [[PYToolboxFeatureRestore alloc] init];
+    option.toolbox.feature.restore.show = YES;
+    option.calculable = NO;
+    PYTreeMapSeries *series = [[PYTreeMapSeries alloc] init];
+    series.name = @"手机占有率";
+    series.type = PYSeriesTypeTreemap;
+    series.itemStyle = [[PYItemStyle alloc] init];
+    PYItemStyleProp *normal = [[PYItemStyleProp alloc] init];
+    normal.label = [[PYLabel alloc] init];
+    normal.label.show = YES;
+    normal.label.formatter = @"{b}";
+    normal.borderWidth = @1;
+    normal.borderColor = PYRGBA(12, 12, 12, 1);
+    series.itemStyle.normal = normal;
+    PYItemStyleProp *emphasis = [[PYItemStyleProp alloc] init];
+    emphasis.label = [[PYLabel alloc] init];
+    emphasis.label.show = YES;
+    emphasis.color = [PYColor colorWithHexString:@"#cc99cc"];
+    emphasis.borderWidth = @3;
+    emphasis.borderColor = [PYColor colorWithHexString:@"#996699"];
+    series.itemStyle.emphasis = emphasis;
+    NSDictionary *data1 = @{@"name":@"三星", @"itemStyle":@{@"normal":@{@"color":@"#99cccc"}}, @"value":@6, @"children":@[@{@"name":@"Galaxy S4", @"value":@2}, @{@"name":@"Galaxy S5", @"value":@3}, @{@"name":@"Galaxy S6", @"value":@3}, @{@"name":@"Galaxy Tab", @"value":@1}]};
+    NSDictionary *data2 = @{@"name":@"小米", @"itemStyle":@{@"normal":@{@"color":@"#99ccff"}}, @"value":@4, @"children":@[@{@"name":@"小米3", @"value":@6}, @{@"name":@"小米4", @"value":@6}, @{@"name":@"红米", @"value":@4}]};
+    NSDictionary *data3 = @{@"name":@"苹果", @"itemStyle":@{@"normal":@{@"color":@"#9999cc"}}, @"value":@4, @"children":@[@{@"name":@"iPhone 5s", @"value":@6}, @{@"name":@"iPhone 6", @"value":@3}, @{@"name":@"iPhone 6+", @"value":@3}]};
+    NSDictionary *data4 = @{@"name":@"魅族", @"itemStyle":@{@"normal":@{@"color":@"#ccff99"}}, @"value":@1, @"children":@[@{@"name":@"MX4", @"itemStyle":@{@"normal":@{@"color":@"#ccccff"}}, @"value":@6}, @{@"name":@"MX3", @"itemStyle":@{@"normal":@{@"color":@"#99ccff"}}, @"value":@6}, @{@"name":@"魅蓝note", @"itemStyle":@{@"normal":@{@"color":@"#9999cc"}}, @"value":@4}, @{@"name":@"MX4 pro", @"itemStyle":@{@"normal":@{@"color":@"#99cccc"}}, @"value":@3}]};
+    NSDictionary *data5 = @{@"name":@"华为", @"itemStyle":@{@"normal":@{@"color":@"#ccffcc"}}, @"value":@2};
+    NSDictionary *data6 = @{@"name":@"联想", @"itemStyle":@{@"normal":@{@"color":@"#ccccff"}}, @"value":@2};
+    NSDictionary *data7 = @{@"name":@"中兴", @"itemStyle":@{@"normal":@{@"color":@"#ffffcc"}}, @"value":@1, @"children":@[@{@"name":@"V5", @"value":@16}, @{@"name":@"努比亚", @"value":@6}, @{@"name":@"功能机", @"value":@4}, @{@"name":@"青漾", @"value":@4}, @{@"name":@"星星", @"value":@4}, @{@"name":@"儿童机", @"value":@1}]};
+    series.data = [[NSMutableArray alloc] initWithArray:@[data1, data2, data3, data4, data5, data6, data7]];
+    option.series = [[NSMutableArray alloc] initWithObjects:series, nil];
+    [_echartsView setOption:option];
 }
 
 - (void)showTreemapDemo3 {
