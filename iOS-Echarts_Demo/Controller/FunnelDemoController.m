6973b54cf92185c7fd6ed35e48b5a1c8ae220503
Pluto Y <kuaileainits@163.com>
2016-08-24 01:19:56 +0800

    Finish the first demo of the Funnel

--- iOS-Echarts_Demo/Controller/FunnelDemoController.m
+++ iOS-Echarts_Demo/Controller/FunnelDemoController.m
@@ -58,7 +58,51 @@ typedef NS_ENUM(NSInteger, FunnelDemoTypeTag) {
 }
 
 - (void)showBasicFunnelDemo1 {
+    PYOption *option = [[PYOption alloc] init];
+    option.title = [[PYTitle alloc] init];
+    option.title.text = @"漏斗图";
+    option.title.subtext = @"纯属虚构";
+    option.tooltip = [[PYTooltip alloc] init];
+    option.tooltip.trigger = PYTooltipTriggerItem;
+    option.tooltip.formatter = @"{a} <br/>{b} : {c}%";
+    option.toolbox = [[PYToolbox alloc] init];
+    option.toolbox.show = YES;
+    option.toolbox = [[PYToolbox alloc] init];
+    option.toolbox.show = YES;
+    option.toolbox.feature = [[PYToolboxFeature alloc] init];
+    option.toolbox.feature.mark = [[PYToolboxFeatureMark alloc] init];
+    option.toolbox.feature.mark.show = YES;
+    option.toolbox.feature.dataView = [[PYToolboxFeatureDataView alloc] init];
+    option.toolbox.feature.dataView.show = YES;
+    option.toolbox.feature.dataView.readOnly = NO;
+    option.toolbox.feature.restore = [[PYToolboxFeatureRestore alloc] init];
+    option.toolbox.feature.restore.show = YES;
     
+    option.legend = [[PYLegend alloc] init];
+    option.legend.data = @[@"展现", @"点击", @"访问", @"咨询", @"订单"];
+    option.calculable = YES;
+    
+    PYFunnelSeries *series = [[PYFunnelSeries alloc] init];
+    series.name = @"漏斗图";
+    series.type = PYSeriesTypeFunnel;
+    series.x = @20;
+    series.width = @"40%";
+    series.data = @[@{@"value":@60, @"name":@"访问"}, @{@"value":@40, @"name":@"咨询"}, @{@"value":@20, @"name":@"订单"}, @{@"value":@80, @"name":@"点击"}, @{@"value":@100, @"name":@"展现"}];
+    
+    PYFunnelSeries *series2 = [[PYFunnelSeries alloc] init];
+    series2.name = @"金字塔";
+    series2.type = PYSeriesTypeFunnel;
+    series2.x = @"55%";
+    series2.width = @"40%";
+    series2.sort = @"ascending";
+    series2.itemStyle = [[PYItemStyle alloc] init];
+    series2.itemStyle.normal = [[PYItemStyleProp alloc] init];
+    series2.itemStyle.normal.label = [[PYLabel alloc] init];
+    series2.itemStyle.normal.label.position = @"left";
+    series2.data = @[@{@"value":@60, @"name":@"访问"}, @{@"value":@40, @"name":@"咨询"}, @{@"value":@20, @"name":@"订单"}, @{@"value":@80, @"name":@"点击"}, @{@"value":@100, @"name":@"展现"}];
+    option.series = [[NSMutableArray alloc] initWithArray:@[series, series2]];
+    
+    [_echartsView setOption:option];
 }
 
 - (void)showMultipleFunnelDemo1 {
