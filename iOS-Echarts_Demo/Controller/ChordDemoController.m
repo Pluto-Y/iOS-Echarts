3fe73be63bc4923259790c2bb2e68c486cfb1d07
Pluto Y <kuaileainits@163.com>
2016-08-25 18:20:37 +0800

    Finish third demo of Chord

--- iOS-Echarts_Demo/Controller/ChordDemoController.m
+++ iOS-Echarts_Demo/Controller/ChordDemoController.m
@@ -134,7 +134,7 @@ typedef NS_ENUM(NSInteger, ChordDemoTypeTag) {
     
     option.tooltip = [[PYTooltip alloc] init];
     option.tooltip.trigger = PYTooltipTriggerItem;
-    option.tooltip.formatter = @"(function (params) {if (params.name && params.name.indexOf(\'-\') != -1) {return params.name.replace(\'-\', \' \' + params.seriesName + \' \');}else {return params.name ? params.name : params.data.id;}})";
+    option.tooltip.formatter = @"(function (params) {if (params.name && params.name.indexOf(\'-\') != -1) {return params.name.replace(\'-\", \' \' + params.seriesName + \' \');}else {return params.name ? params.name : params.data.id;}})";
     option.legend = [[PYLegend alloc] init];
     option.legend.data = @[@"美国", @"叙利亚反对派", @"阿萨德", @"伊朗", @"塞西", @"哈马斯", @"以色列", @"穆斯林兄弟会", @"基地组织", @"俄罗斯", @"黎巴嫩什叶派", @"土耳其", @"卡塔尔", @"沙特", @"黎巴嫩逊尼派", @"", @"支持", @"反对", @"未表态"];
     option.legend.orient = @"vertical";
@@ -210,7 +210,93 @@ typedef NS_ENUM(NSInteger, ChordDemoTypeTag) {
 }
 
 - (void)showBasicChordDemo2 {
+    PYOption *option = [[PYOption alloc] init];
+    option.title = [[PYTitle alloc] init];
+    option.title.text = @"德国队效力联盟";
+    option.title.x = @"right";
+    option.title.y = @"bottom";
+    option.tooltip = [[PYTooltip alloc] init];
+    option.tooltip.trigger = PYTooltipTriggerItem;
+    option.tooltip.formatter = @"(function (params) {if (params.indicator2) {return params.indicator2 + \' \' + params.name + \' \' + params.indicator;} else {return params.name}})";
+    option.toolbox = [[PYToolbox alloc] init];
+    option.toolbox.show = YES;
+    option.toolbox.feature = [[PYToolboxFeature alloc] init];
+    option.toolbox.feature.restore = [[PYToolboxFeatureRestore alloc] init];
+    option.toolbox.feature.restore.show = YES;
+    option.toolbox.feature.magicType = [[PYToolboxFeatureMagicType alloc] init];
+    option.toolbox.feature.magicType.show = YES;
+    option.toolbox.feature.magicType.type = @[PYSeriesTypeForce, PYSeriesTypeChord];
+    option.toolbox.feature.restore = [[PYToolboxFeatureRestore alloc] init];
+    option.toolbox.feature.restore.show = YES;
     
+    option.legend = [[PYLegend alloc] init];
+    option.legend.x = @"left";
+    option.legend.data = @[@"阿森纳", @"拜仁慕尼黑", @"多特蒙德"];
+    
+    PYChordSeries *series = [[PYChordSeries alloc] init];
+    series.type = PYSeriesTypeChord;
+    series.sort = PYSortAscending;
+    series.sortSub = PYSortDescending;
+    series.showScale = NO;
+    series.itemStyle = [[PYItemStyle alloc] init];
+    series.itemStyle.normal = [[PYItemStyleProp alloc] init];
+    series.itemStyle.normal.label = [[PYLabel alloc] init];
+    series.itemStyle.normal.label.rotate = YES;
+    series.nodes = @[
+                     @{@"name":@"默特萨克"},
+                     @{@"name":@"厄齐尔"},
+                     @{@"name":@"波多尔斯基"},
+                     @{@"name":@"诺伊尔"},
+                     @{@"name":@"博阿滕"},
+                     @{@"name":@"施魏因施泰格"},
+                     @{@"name":@"拉姆"},
+                     @{@"name":@"克罗斯"},
+                     @{@"name":@"穆勒"},
+                     @{@"name":@"格策"},
+                     @{@"name":@"胡梅尔斯"},
+                     @{@"name":@"魏登费勒"},
+                     @{@"name":@"杜尔姆"},
+                     @{@"name":@"格罗斯克罗伊茨"},
+                     @{@"name":@"阿森纳"},
+                     @{@"name":@"拜仁慕尼黑"},
+                     @{@"name":@"多特蒙德"}
+                     ];
+    
+    series.links = @[
+                     @{@"source":@"阿森纳", @"target":@"默特萨克", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"阿森纳", @"target":@"厄齐尔", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"阿森纳", @"target":@"波多尔斯基", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"拜仁慕尼黑", @"target":@"诺伊尔", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"拜仁慕尼黑", @"target":@"博阿滕", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"拜仁慕尼黑", @"target":@"施魏因施泰格", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"拜仁慕尼黑", @"target":@"拉姆", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"拜仁慕尼黑", @"target":@"克罗斯", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"拜仁慕尼黑", @"target":@"穆勒", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"拜仁慕尼黑", @"target":@"格策", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"多特蒙德", @"target":@"胡梅尔斯", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"多特蒙德", @"target":@"魏登费勒", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"多特蒙德", @"target":@"杜尔姆", @"weight": @0.9, @"name":@"效力"},
+                     @{@"source":@"多特蒙德", @"target":@"格罗斯克罗伊茨", @"weight": @0.9, @"name":@"效力"},
+                     
+                     // Ribbon Type 的和弦图每一对节点之间必须是双向边
+                     @{@"target":@"阿森纳", @"source":@"默特萨克", @"weight": @1},
+                     @{@"target":@"阿森纳", @"source":@"厄齐尔", @"weight": @1},
+                     @{@"target":@"阿森纳", @"source":@"波多尔斯基", @"weight": @1},
+                     @{@"target":@"拜仁慕尼黑", @"source":@"诺伊尔", @"weight": @1},
+                     @{@"target":@"拜仁慕尼黑", @"source":@"博阿滕", @"weight": @1},
+                     @{@"target":@"拜仁慕尼黑", @"source":@"施魏因施泰格", @"weight": @1},
+                     @{@"target":@"拜仁慕尼黑", @"source":@"拉姆", @"weight": @1},
+                     @{@"target":@"拜仁慕尼黑", @"source":@"克罗斯", @"weight": @1},
+                     @{@"target":@"拜仁慕尼黑", @"source":@"穆勒", @"weight": @1},
+                     @{@"target":@"拜仁慕尼黑", @"source":@"格策", @"weight": @1},
+                     @{@"target":@"多特蒙德", @"source":@"胡梅尔斯", @"weight": @1},
+                     @{@"target":@"多特蒙德", @"source":@"魏登费勒", @"weight": @1},
+                     @{@"target":@"多特蒙德", @"source":@"杜尔姆", @"weight": @1},
+                     @{@"target":@"多特蒙德", @"source":@"格罗斯克罗伊茨", @"weight": @1}
+                     ];
+    option.series = [[NSMutableArray alloc] initWithArray:@[series]];
+    
+    [_echartsView setOption:option];
 }
 
 - (void)showNonRibbonChordDemo {
