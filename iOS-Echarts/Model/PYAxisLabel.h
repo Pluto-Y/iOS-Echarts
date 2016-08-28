c7fb5bac5fee87757c68cb7432307d94f55a9ba5
Pluto Y <kuaileainits@163.com>
2016-08-28 22:36:19 +0800

    Update PYAxisLabel for method chaining

--- iOS-Echarts/Model/PYAxisLabel.h
+++ iOS-Echarts/Model/PYAxisLabel.h
@@ -7,7 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "PYTextStyle.h"
+
+@class PYTextStyle;
 
 /**
  *
--- iOS-Echarts/Model/PYAxisLabel.m
+++ iOS-Echarts/Model/PYAxisLabel.m
@@ -7,6 +7,7 @@
 //
 
 #import "PYAxisLabel.h"
+#import "PYTextStyle.h"
 
 @implementation PYAxisLabel
 
