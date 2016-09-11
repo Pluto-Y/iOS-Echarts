62470f6a52114d92f6b2454312c4fd18519c0609
Pluto Y <kuaileainits@163.com>
2016-09-12 01:42:34 +0800

    Rewrite demo for MultipleAngularGauges

--- DemoOptions/PYGaugesDemoOptions.m
+++ DemoOptions/PYGaugesDemoOptions.m
@@ -194,7 +194,191 @@
 }
 
 + (PYOption *)multipleAngularGauges1Option {
-    return nil;
+    return [PYOption initPYOptionWithBlock:^(PYOption *option) {
+        option.tooltipEqual([PYTooltip initPYTooltipWithBlock:^(PYTooltip *tooltip) {
+            tooltip.formatterEqual(@"{a} <br/>{b} : {c}%");
+        }])
+        .toolboxEqual([PYToolbox initPYToolboxWithBlock:^(PYToolbox *toolbox) {
+            toolbox.showEqual(YES)
+            .featureEqual([PYToolboxFeature initPYToolboxFeatureWithBlock:^(PYToolboxFeature *feature) {
+                feature.markEqual([PYToolboxFeatureMark initPYToolboxFeatureMarkWithBlock:^(PYToolboxFeatureMark *mark) {
+                    mark.show = YES;
+                }])
+                .restoreEqual([PYToolboxFeatureRestore initPYToolboxFeatureRestoreWithBlock:^(PYToolboxFeatureRestore *restore) {
+                    restore.showEqual(YES);
+                }]);
+            }]);
+        }])
+        .addSeries([PYGaugeSeries initPYGaugeSeriesWithBlock:^(PYGaugeSeries *series) {
+            series.minEqual(@0)
+            .maxEqual(@220)
+            .radiusEqual(@"60%")
+            .splitNumberEqual(@11)
+            .axisLineEqual([PYAxisLine initPYAxisLineWithBlock:^(PYAxisLine *axisLine) {
+                axisLine.lineStyleEqual([PYLineStyle initPYLineStyleWithBlock:^(PYLineStyle *lineStyle) {
+                    lineStyle.widthEqual(@10);
+                }]);
+            }])
+            .axisTickEqual([PYAxisTick initPYAxisTickWithBlock:^(PYAxisTick *axisTick) {
+                axisTick.lengthEqual(@15)
+                .lineStyleEqual([PYLineStyle initPYLineStyleWithBlock:^(PYLineStyle *lineStyle) {
+                    lineStyle.colorEqual(@"auto");
+                }]);
+            }])
+            .splitLineEqual([PYGaugeSpliteLine initPYGaugeSpliteLineWithBlock:^(PYGaugeSpliteLine *spliteLine) {
+                spliteLine.lengthEqual(@20);
+            }])
+            .titleEqual([PYGaugeTitle initPYGaugeTitleWithBlock:^(PYGaugeTitle *title) {
+                title.offsetCenterEqual(@[@"0", @"-35%"])
+                .textStyleEqual([PYTextStyle initPYTextStyleWithBlock:^(PYTextStyle *textStyle) {
+                    textStyle.fontWeightEqual(PYTextStyleFontWeightBolder)
+                    .fontSizeEqual(@12)
+                    .fontStyleEqual(PYTextStyleFontStyleItalic);
+                }]);
+            }])
+            .detailEqual([PYGaugeDetail initPYGaugeDetailWithBlock:^(PYGaugeDetail *detail) {
+                detail.textStyleEqual([PYTextStyle initPYTextStyleWithBlock:^(PYTextStyle *textStyle) {
+                    textStyle.fontWeightEqual(PYTextStyleFontWeightBolder);
+                }]);
+            }])
+            .zEqual(@3)
+            .nameEqual(@"速度")
+            .typeEqual(PYSeriesTypeGauge)
+            .addData(@{@"value":@40, @"name":@"km/h"});
+        }])
+        .addSeries([PYGaugeSeries initPYGaugeSeriesWithBlock:^(PYGaugeSeries *series) {
+            series.centerEqual(@[@"15%", @"55%"])
+            .radiusEqual(@"45%")
+            .minEqual(@0)
+            .maxEqual(@7)
+            .endAngleEqual(@45)
+            .splitNumberEqual(@7)
+            .axisLineEqual([PYAxisLine initPYAxisLineWithBlock:^(PYAxisLine *axisLine) {
+                axisLine.lineStyleEqual([PYLineStyle initPYLineStyleWithBlock:^(PYLineStyle *lineStyle) {
+                    lineStyle.widthEqual(@8);
+                }]);
+            }])
+            .axisTickEqual([PYAxisTick initPYAxisTickWithBlock:^(PYAxisTick *axisTick) {
+                axisTick.lengthEqual(@12)
+                .lineStyleEqual([PYLineStyle initPYLineStyleWithBlock:^(PYLineStyle *lineStyle) {
+                    lineStyle.colorEqual(@"auto");
+                }]);
+            }])
+            .splitLineEqual([PYGaugeSpliteLine initPYGaugeSpliteLineWithBlock:^(PYGaugeSpliteLine *spliteLine) {
+                spliteLine.lengthEqual(@20)
+                .lineStyleEqual([PYLineStyle initPYLineStyleWithBlock:^(PYLineStyle *lineStyle) {
+                    lineStyle.colorEqual(@"auto");
+                }]);
+            }])
+            .pointerEqual([PYGaugePointer initPYGaugePointerWithBlock:^(PYGaugePointer *pointer) {
+                pointer.widthEqual(@5);
+            }])
+            .titleEqual([PYGaugeTitle initPYGaugeTitleWithBlock:^(PYGaugeTitle *title) {
+                title.offsetCenterEqual(@[@0, @"-20%"])
+                .textStyleEqual([PYTextStyle initPYTextStyleWithBlock:^(PYTextStyle *textStyle) {
+                    textStyle.fontSizeEqual(@8);
+                }]);
+            }])
+            .detailEqual([PYGaugeDetail initPYGaugeDetailWithBlock:^(PYGaugeDetail *detail) {
+                detail.textStyleEqual([PYTextStyle initPYTextStyleWithBlock:^(PYTextStyle *textStyle) {
+                    textStyle.fontWeightEqual(PYTextStyleFontWeightBolder);
+                }]);
+            }])
+            .nameEqual(@"转速")
+            .typeEqual(PYSeriesTypeGauge)
+            .addData(@{@"value":@1.5, @"name":@"x1000 r/min"});
+        }])
+        .addSeries([PYGaugeSeries initPYGaugeSeriesWithBlock:^(PYGaugeSeries *series) {
+            series.radiusEqual(@"45%")
+            .minEqual(@0)
+            .maxEqual(@2)
+            .startAngleEqual(@135)
+            .endAngleEqual(@45)
+            .splitNumberEqual(@2)
+            .axisLineEqual([PYAxisLine initPYAxisLineWithBlock:^(PYAxisLine *axisLine) {
+                axisLine.lineStyleEqual([PYLineStyle initPYLineStyleWithBlock:^(PYLineStyle *lineStyle) {
+                    lineStyle.colorEqual(@[
+                                           @[@0.2, [PYColor colorWithHexString:@"#ff4500"]],
+                                           @[@0.8, [PYColor colorWithHexString:@"#48b"]],
+                                           @[@1, [PYColor colorWithHexString:@"#228b22"]]
+                                           ])
+                    .widthEqual(@8);
+                }]);
+            }])
+            .axisTickEqual([PYAxisTick initPYAxisTickWithBlock:^(PYAxisTick *axisTick) {
+                axisTick.splitNumberEqual(@5)
+                .lengthEqual(@10)
+                .lineStyleEqual([PYLineStyle initPYLineStyleWithBlock:^(PYLineStyle *lineStyle) {
+                    lineStyle.colorEqual(@"auto");
+                }]);
+            }])
+            .axisLabelEqual([PYAxisLabel initPYAxisLabelWithBlock:^(PYAxisLabel *axisLabel) {
+                axisLabel.formatterEqual(@"(function(v){switch (v + \'\') {case \'0\' : return \'E\';case \'1\' : return \'Gas\';case \'2\' : return \'F\';}})");
+            }])
+            .splitLineEqual([PYGaugeSpliteLine initPYGaugeSpliteLineWithBlock:^(PYGaugeSpliteLine *spliteLine) {
+                spliteLine.lengthEqual(@15)
+                .lineStyleEqual([PYLineStyle initPYLineStyleWithBlock:^(PYLineStyle *lineStyle) {
+                    lineStyle.colorEqual(@"auto");
+                }]);
+            }])
+            .pointerEqual([PYGaugePointer initPYGaugePointerWithBlock:^(PYGaugePointer *pointer) {
+                pointer.widthEqual(@2);
+            }])
+            .titleEqual([PYGaugeTitle initPYGaugeTitleWithBlock:^(PYGaugeTitle *title) {
+                title.showEqual(NO);
+            }])
+            .detailEqual([PYGaugeDetail initPYGaugeDetailWithBlock:^(PYGaugeDetail *detail) {
+                detail.showEqual(NO);
+            }])
+            .centerEqual(@[@"85%", @"50%"])
+            .typeEqual(PYSeriesTypeGauge)
+            .nameEqual(@"油表")
+            .addData(@{@"value":@0.5, @"name":@"gas"});
+        }])
+        .addSeries([PYGaugeSeries initPYGaugeSeriesWithBlock:^(PYGaugeSeries *series) {
+            series.centerEqual(@[@"85%", @"50%"])
+            .radiusEqual(@"45%")
+            .minEqual(@0)
+            .maxEqual(@2)
+            .startAngleEqual(@315)
+            .endAngleEqual(@225)
+            .splitNumberEqual(@2)
+            .axisLineEqual([PYAxisLine initPYAxisLineWithBlock:^(PYAxisLine *axisLine) {
+                axisLine.lineStyleEqual([PYLineStyle initPYLineStyleWithBlock:^(PYLineStyle *lineStyle) {
+                    lineStyle.colorEqual(@[
+                                           @[@0.2, [PYColor colorWithHexString:@"#ff4500"]],
+                                           @[@0.8, [PYColor colorWithHexString:@"#48b"]],
+                                           @[@1, [PYColor colorWithHexString:@"#228b22"]]
+                                           ])
+                    .widthEqual(@8);
+                }]);
+            }])
+            .axisTickEqual([PYAxisTick initPYAxisTickWithBlock:^(PYAxisTick *axisTick) {
+                axisTick.showEqual(NO);
+            }])
+            .axisLabelEqual([PYAxisLabel initPYAxisLabelWithBlock:^(PYAxisLabel *axisLabel) {
+                axisLabel.formatterEqual(@"(function(v){switch (v + \'\') {case \'0\' : return \'H\';case \'1\' : return \'Water\';case \'2\' : return \'C\';}})");
+            }])
+            .splitLineEqual([PYGaugeSpliteLine initPYGaugeSpliteLineWithBlock:^(PYGaugeSpliteLine *spliteLine) {
+                spliteLine.lengthEqual(@15)
+                .lineStyleEqual([PYLineStyle initPYLineStyleWithBlock:^(PYLineStyle *lineStyle) {
+                    lineStyle.colorEqual(@"auto");
+                }]);
+            }])
+            .pointerEqual([PYGaugePointer initPYGaugePointerWithBlock:^(PYGaugePointer *pointer) {
+                pointer.widthEqual(@2);
+            }])
+            .titleEqual([PYGaugeTitle initPYGaugeTitleWithBlock:^(PYGaugeTitle *title) {
+                title.showEqual(NO);
+            }])
+            .detailEqual([PYGaugeDetail initPYGaugeDetailWithBlock:^(PYGaugeDetail *detail) {
+                detail.showEqual(NO);
+            }])
+            .nameEqual(@"水表")
+            .typeEqual(PYSeriesTypeGauge)
+            .addData(@{@"value":@0.5, @"name":@"gas"});
+        }]);
+    }];
 }
 
 + (PYOption *)multipleAngularGauges2Option {
